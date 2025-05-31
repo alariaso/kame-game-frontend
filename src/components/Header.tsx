10f50b5d2d4de12626d4cdeb1497df90e20aa5e5
alariaso <141962262+alariaso@users.noreply.github.com>
2025-05-31 11:20:51 -0500

    implementar otro tipo de botón

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,13 +24,13 @@ export const Header: React.FC = () => {
             </ul>
             <ul className="flex gap-5 items-center">
                 {user === null ? <>
-                    <li className="p-2"><Link to="/login">Iniciar Sesión</Link></li>
+                    <li className="p-3"><Link to="/login">Iniciar Sesión</Link></li>
                     <li><Button component={Link} to="/registro">Crear Cuenta</Button></li>
                 </> : <>
                     <li className="text-yellow-400 p-2"><Wallet className="inline-block align-text-bottom" size={20} /> {user.yugiPesos} YP</li>
-                    <li><Link to="/carrito" className="border-1 border-yellow-400 text-yellow-400 p-2 rounded-sm block"><ShoppingCart size={20} /></Link></li>
+                    <li><Link to="/carrito" className="border-1 border-yellow-400 text-yellow-400 p-3 rounded-sm block"><ShoppingCart size={20} /></Link></li>
                     <li className="p-2">Hola, {user.username}</li>
-                    <li onClick={handleLogout} className="cursor-pointer text-yellow-400 border-yellow-400 border-1 p-2 px-4 rounded-lg"><LogOut size={20} className="inline-block align-text-bottom" /> Salir</li>
+                    <li onClick={handleLogout}><Button kind="outline"><LogOut size={20} className="inline-block align-text-bottom" /> Salir</Button></li>
                 </>}
             </ul>
         </nav>
--- src/elements/Button.tsx
+++ src/elements/Button.tsx
@@ -1,9 +1,20 @@
+type ButtonKind = 'fill' | 'outline';
+
 type ButtonProps<C extends React.ElementType> = {
     component?: C;
+    kind?: ButtonKind
+    className?: string;
 } & React.ComponentPropsWithoutRef<C>;
 
-export const Button = <C extends React.ElementType = 'button'>({component, ...restProps}: ButtonProps<C>) => {
-    const Component = component || 'button';
+const classes = {
+    "fill": "cursor-pointer px-6 py-3 bg-yellow-400 text-black rounded-lg",
+    "outline": "cursor-pointer px-6 py-3 border-1 border-yellow-400 text-yellow-400 rounded-lg"
+}
 
-    return <Component className="px-6 py-3 bg-yellow-400 text-black rounded-lg" {...restProps} />
+export const Button = <C extends React.ElementType = 'button'>({component, kind, className, ...restProps}: ButtonProps<C>) => {
+    const Component = component || 'button';
+    const k = kind ? kind : "fill";
+    const c = className ? `${classes[k]} ${className}` : classes[k]
+
+    return <Component className={c} {...restProps} />
 }
