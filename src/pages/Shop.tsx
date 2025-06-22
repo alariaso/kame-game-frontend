5947c9e0c860265106b7d752b345dcca506b35ee
secadavida <secadavida@unal.edu.co>
2025-06-21 23:06:28 -0500

    fix(Shop): change title

--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -79,7 +79,7 @@ const Shop: React.FC = () => {
 	return (
 		<div className="container mx-auto py-6 px-4 sm:px-6">
 			<h1 className="text-2xl sm:text-3xl font-bold text-gold mb-6">
-				Tienda de Duelo
+				Tienda de Cartas
 			</h1>
 
 			{/* search bar and filters */}
